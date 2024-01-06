5a6872f9e559f8ee533b7888efa0ccc661ee824f
anuragkothari1 <anukothari111@gmail.com>
2024-01-06 21:30:06 +0530

    change

---
+++ script.js
@@ -0,0 +1,15 @@
+
+const menuClicked=()=>{
+    const menu=document.getElementById("formobile");
+    menu.style.display="flex";
+  
+    console.log("clicked");
+
+}
+
+const closeClicked=()=>{
+    const close=document.getElementById("formobile");
+    close.style.display="none";
+
+    console.log("clicked");
+}
